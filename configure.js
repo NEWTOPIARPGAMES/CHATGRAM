9b2c5e8244c34fc8a12fabb6f824e9133996dc09
NEWTOPIARPGAMES <137911644+NEWTOPIARPGAMES@users.noreply.github.com>
2024-04-27 10:15:49 +0200

    Delete configure.js

--- configure.js
+++
@@ -1,9 +0,0 @@
-let signup = document.getElementById("signup")
-signup.onclick = function(){
-    window.location.href = 'home.html';
-}
-
-let download = document.getElementById("download")
-download.onclick = function(){
-    window.alert("sei per essere rendirizzato a github.com");
-}
